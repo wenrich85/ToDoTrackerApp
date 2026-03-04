6afb976dedf7cbff70e35c9ad44adfd036555525
Wendell Richards <wenrich85@gmail.com>
2026-03-03 20:34:05 -0600

    Initial Commit

---
+++ ToDoTracker/ToDoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTrackerApp.swift
+//  ToDoTracker
+//
+//  Created by Wendell Richards on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoTrackerTests/ToDoTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTrackerTests.swift
+//  ToDoTrackerTests
+//
+//  Created by Wendell Richards on 3/3/26.
+//
+
+import Testing
+@testable import ToDoTracker
+
+struct ToDoTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
